b314d10ba2aba9212c362222b17764ab42d8ea9f
naoeoneto <abdpneto@gmail.com>
2023-05-25 12:19:41 -0300

    class 9

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UsersRepository } from './repositories/users.repository';
-import { UsersInMemoryRepository } from './repositories/in-memory/users.in-memory.repository';
+
+import { PrismaService } from 'src/database/prisma.service';
+import { UsersPrismaRepository } from './repositories/prisma/users.prisma.repository';
 
 @Module({
   controllers: [UsersController],
   providers: [
     UsersService,
-    { provide: UsersRepository, useClass: UsersInMemoryRepository },
+    PrismaService,
+    { provide: UsersRepository, useClass: UsersPrismaRepository },
   ],
 })
 export class UsersModule {}
